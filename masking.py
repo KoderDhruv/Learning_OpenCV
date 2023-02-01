d9645b918e59c79a03310764cfa659dad2cddae9
KoderDhruv <dhruvbanuni@gmail.com>
2023-02-01 20:26:09 +0530

    Update masking.py

--- masking.py
+++ masking.py
@@ -1,12 +1,16 @@
 import cv2 as cv
 import numpy as np
 
-img=cv.imread('Photos/cats.jpg')
+img = cv.imread('Photos/cats_2.jpg')
 cv.imshow('Cats', img)
 
-blank=np.zeros(img.shape[:2], dtype='uint8')
+blank = np.zeros(img.shape[:2], dtype='uint8')
 cv.imshow('Blank', blank)
 
-mask=cv.circle(blank, )
+circle_mask = cv.circle(blank, (img.shape[1]//2, img.shape[0]//2), 100, 255, -1)
+cv.imshow('Mask', circle_mask)
+
+masked = cv.bitwise_and(img, img, mask=circle_mask)
+cv.imshow('Masked_Image', masked)
 
 cv.waitKey(0)
