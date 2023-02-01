4461ea172cc00e2c797e9a43b4169b71caeb2810
KoderDhruv <dhruvbanuni@gmail.com>
2023-02-01 22:40:18 +0530

    Update edge_detection.py

--- edge_detection.py
+++ edge_detection.py
@@ -12,4 +12,16 @@ lap = cv.Laplacian(gray, cv.CV_64F)
 lap = np.uint8(np.absolute(lap))
 cv.imshow('Laplacian', lap)
 
+# Sobel
+sobelx=cv.Sobel(gray, cv.CV_64F, 1, 0)
+sobely=cv.Sobel(gray, cv.CV_64F, 0, 1)
+combined_sobel=cv.bitwise_or(sobelx, sobely)
+
+cv.imshow('Sobelx', sobelx)
+cv.imshow('Sobely', sobely)
+cv.imshow('Combined_sobel', combined_sobel)
+
+canny=cv.Canny(gray, 150, 175)
+cv.imshow('Canny', canny)
+
 cv.waitKey(0)
